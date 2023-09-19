18c68563501a79e5436aa6a1892b427e97b09729
Ruel Cabaluna Jr <rcabalunajr@gmail.com>
2023-09-19 12:02:52 +0800

    Added modal for confirm attendance/delete function

--- handa/app/Models/DashboardModel.php
+++ handa/app/Models/DashboardModel.php
@@ -10,7 +10,7 @@ class DashboardModel extends Model
     public function get_par_att_data(){
 
 
-        $sql = "SELECT a.name,a.shorthand, (SELECT COUNT(*) FROM tblparticipants b WHERE b.event = a.shorthand) AS participantsno, (SELECT COUNT(*) FROM tblattendance c WHERE c.event = a.shorthand) AS attendanceno FROM tblevents a GROUP BY a.shorthand ORDER BY a.eventid";
+        $sql = "SELECT a.name,a.shorthand, (SELECT COUNT(b.participantid) FROM tblparticipants b WHERE b.event = a.shorthand) AS participantsno, (SELECT COUNT(*) FROM tblattendance c WHERE c.event = a.shorthand) AS attendanceno FROM tblevents a GROUP BY a.shorthand ORDER BY a.eventid";
         $query = $this->db->query($sql);
 
         return $query->getResultArray();
