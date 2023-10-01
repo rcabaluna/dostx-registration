0c6a9fc15c0fc234e900020f27e3a2d1306adb43
Ruel <rcabalunajr@gmail.com>
2023-10-02 04:28:09 +0800

    Added Attendance Search By User

--- handa/app/Views/admin/attendance/search-user.php
+++ handa/app/Views/admin/attendance/search-user.php
@@ -12,7 +12,7 @@
                 <div class="card-body">
                     <div class="align-items-center justify-content-between m-b-1">
                         <div class="row mb-3">
-                            <div class="col-12"><img class="img-fluid" src="<?=base_url("assets/images/logo/walk-in-reg-header.png");?>"></div>
+                            <div class="col-12"><img class="img-fluid" src="<?= base_url("assets/images/logo/walk-in-reg-header.png") ?>"></div>
                         </div>
                     </div>
                     <hr />
@@ -40,82 +40,86 @@
                             </thead>
                             <tbody>
                                 <?php if ($participants) {
-                                 $count=0; foreach ($participants as $participantsRow) {
-                                                        ?>
-                                <tr participantid="<?=$participantsRow['participantid']?>" shorthand="<?=$participantsRow['shorthand']?>">
-                                    <td><?=$count+=1?></td>
-                                    <td><?=$participantsRow['regnumber']?></td>
-                                    <td><?=$participantsRow['title']?></td>
+                                    $count = 0;
+                                    foreach ($participants as $participantsRow) { ?>
+                                <tr participantid="<?= $participantsRow['participantid'] ?>" shorthand="<?= $participantsRow['shorthand'] ?>">
+                                    <td><?= $count += 1 ?></td>
+                                    <td><?= $participantsRow['regnumber'] ?></td>
+                                    <td><?= $participantsRow['title'] ?></td>
                                     <td>
                                         <?php
-                                            echo $participantsRow['lastname'].', '.$participantsRow['firstname'];
-                                            echo ($participantsRow['suffix'] ? ", ".$participantsRow['suffix'] : '');
-                                            echo ($participantsRow['middle_initial'] ? " ".$participantsRow['middle_initial'] : '');
-                                            ?>
+                                        echo $participantsRow['lastname'] . ', ' . $participantsRow['firstname'];
+                                        echo $participantsRow['suffix'] ? ", " . $participantsRow['suffix'] : '';
+                                        echo $participantsRow['middle_initial'] ? " " . $participantsRow['middle_initial'] : '';
+                                        ?>
                                     </td>
-                                    <td><?=$participantsRow['contactno']?></td>
-                                    <td><?=$participantsRow['email']?></td>
-                                    <td><?=$participantsRow['sex']?></td>
+                                    <td><?= $participantsRow['contactno'] ?></td>
+                                    <td><?= $participantsRow['email'] ?></td>
+                                    <td><?= $participantsRow['sex'] ?></td>
                                     <td>
-                                        <small><?=$participantsRow['regDesc']." - <br>".$participantsRow['provDesc']?></small>
+                                        <small><?= $participantsRow['regDesc'] . " - <br>" . $participantsRow['provDesc'] ?></small>
                                     </td>
-                                    <td><?=$participantsRow['agency_name']?></td>
-                                    <td><?=$participantsRow['position']?></td>
-                                    <td><?=$participantsRow['sectorname']?></td>
+                                    <td><?= $participantsRow['agency_name'] ?></td>
+                                    <td><?= $participantsRow['position'] ?></td>
+                                    <td><?= $participantsRow['sectorname'] ?></td>
                                     <td>
-                                        <small><?=$participantsRow['name']?></small>
+                                        <small><?= $participantsRow['name'] ?></small>
                                     </td>
-                                    <td><?=($participantsRow['privileges']) ? $participantsRow['privileges'] : '-' ?></td>
-                                    <td><?=date("M d, Y h:i A",strtotime($participantsRow['date_registered'].'+8 hours'))?></td>
+                                    <td><?= $participantsRow['privileges'] ? $participantsRow['privileges'] : '-' ?></td>
+                                    <td><?= date("M d, Y h:i A", strtotime($participantsRow['date_registered'] . '+8 hours')) ?></td>
                                     <!-- <td>
-                                                        <button type="button" class="btn btn-danger btn-rounded btn-tone btn-xs" onclick="set_delete_link(<?=$participantsRow['participantid']; ?>)" data-toggle="modal" data-target="#exampleModal">
+                                                        <button type="button" class="btn btn-danger btn-rounded btn-tone btn-xs" onclick="set_delete_link(<?= $participantsRow[
+                                                            'participantid'
+                                                        ] ?>)" data-toggle="modal" data-target="#exampleModal">
                                                             <i class="anticon anticon-delete"></i>
                                                         </button>
                                                 </td> -->
                                 </tr>
-                                <?php }  
-                                }  ?>
+                                <?php }
+                                } ?>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="notification-toast top-right" id="notification-toast"></div>
+
     </div>
 
     <!-- Modal -->
-    <div class="modal fade" id="search-user-mdl">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Search Participant</h5>
-                    <button type="button" class="close" data-dismiss="modal">
-                        <i class="anticon anticon-close"></i>
-                    </button>
-                </div>
-                <form>
+<div class="modal fade" id="search-user-mdl">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Search Participant</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <i class="anticon anticon-close"></i>
+                </button>
+            </div>
+            <form>
                 <div class="modal-body">
-                    
-                        <div class="form-group">
+                    <div class="form-row">
+                        <div class="form-group col-md-12">
                             <label for="formGroupExampleInput">Last Name</label>
-                            <input type="text" name="lastname" class="form-control" id="formGroupExampleInput" placeholder="Enter Last Name">
+                            <input type="text" name="lastname" class="form-control" id="formGroupExampleInput" placeholder="Enter Last Name" />
                         </div>
-                        <div class="form-group">
+                        <div class="form-group col-md-12">
                             <label for="formGroupExampleInput2">First Name</label>
-                            <input type="text" name="firstname" class="form-control" id="formGroupExampleInput2" placeholder="Enter First Name">
+                            <input type="text" name="firstname" class="form-control" id="formGroupExampleInput2" placeholder="Enter First Name" />
                         </div>
-                    
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
                     <button type="submit" class="btn btn-danger">Search</button>
                 </div>
-                </form>
-            </div>
+            </form>
         </div>
     </div>
+</div>
 
+
+        <div class="notification-toast top-right" id="notification-toast"></div>
     <div class="modal fade" id="options-mdl">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -146,8 +150,12 @@
     </div>
     <div class="notification-toast top-right" id="notification-toast"></div>
 </div>
-<?php if(isset($_SESSION['exists'])){ echo '<script>show_notification_exists("attendance");</script>'; } ?>    
-<?php if(isset($_SESSION['confirmed'])){ echo '<script>show_notification_confirmed("attendance");</script>'; } ?>    
+<?php if (isset($_SESSION['exists'])) {
+    echo '<script>show_notification_exists("attendance");</script>';
+} ?>    
+<?php if (isset($_SESSION['confirmed'])) {
+    echo '<script>show_notification_confirmed("attendance");</script>';
+} ?>    
 <script type="text/javascript">
 
     $(document).ready(function () {
@@ -173,10 +181,10 @@
             
             $("#regnumber-options-mdl").html(data[3]);
             $("#event-title-options-mdl").html(data[11]);
-            $("#qr-options-mdl").attr("src", "<?=base_url('uploads/qr/')?>"+data[1]+".png");
+            $("#qr-options-mdl").attr("src", "<?= base_url('uploads/qr/') ?>"+data[1]+".png");
             $("#regnumber").val(data[1]);
             $("#event").val(shorthand);
-            $("#confirm-att-by-search").attr("action","<?=base_url('attendance/confirm-att-search')?>");
+            $("#confirm-att-by-search").attr("action","<?= base_url('attendance/confirm-att-search') ?>");
             $("#options-mdl").modal("show");
         });
 
