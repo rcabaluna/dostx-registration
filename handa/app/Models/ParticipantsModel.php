b7d18f32344e4b04fe8907484e0a052a0626fe1c
Ruel <rcabalunajr@gmail.com>
2023-10-03 03:58:50 +0800

    Added Replication of Data for registration in another forum

--- handa/app/Models/ParticipantsModel.php
+++ handa/app/Models/ParticipantsModel.php
@@ -18,20 +18,20 @@ class ParticipantsModel extends Model
     public function get_participants_list($tablename,$param){
 
 
-        $builder = $this->db->table($tablename);
-        $builder->select('*');
-        $builder->join('tblsector', 'tblsector.sectorid = tblparticipants.sector');
-        $builder->join('refregion', 'refregion.regCode = tblparticipants.address_region');
-        $builder->join('refprovince', 'refprovince.provCode = tblparticipants.address_province');
-        $builder->join('tblevents', 'tblevents.shorthand = tblparticipants.event');
+        $query = $this->db->table('tblparticipants a');
+            $query->select('a.*, b.sectorname, c.regDesc,e.name');
+            $query->join('tblsector b', 'b.sectorid = a.sector');
+            $query->join('refregion c', 'c.regCode = a.address_region');
+            $query->join('refprovince d', 'd.provCode = a.address_province');
+            $query->join('tblevents e', 'e.shorthand = a.event');
+            
+            if ($param['event'] != 'all' && $param['event'] != '') {
+                $query->where($param);
+            }
+            $query->groupBy('a.regnumber');
+            $query->orderBy('a.participantid', 'DESC');
 
-        if ($param['event'] != 'all' && $param['event'] != '') {
-            $builder->where($param);
-        }
-        $query = $builder->orderBy('tblparticipants.participantid','DESC');
-        $query = $builder->get();
-
-        return $query->getResultArray();
+        return $query->get()->getResultArray();
     }
 
     public function delete_participant($tablename,$param){
