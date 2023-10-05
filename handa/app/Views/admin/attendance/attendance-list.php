617190f4619d19eba5c01d340f574db7de7e50b8
Ruel <rcabalunajr@gmail.com>
2023-10-05 21:35:53 +0800

    Added multiple attendance in DRRM Exhibits

--- handa/app/Views/admin/attendance/attendance-list.php
+++ handa/app/Views/admin/attendance/attendance-list.php
@@ -33,8 +33,8 @@
                             <th>Sex</th>
                             <th>Address</th>
                             <th>Agency Name</th>
-                            <th>Sector/Affiliation</th>
                             <th>Position</th>
+                            <th>Sector/Affiliation</th>
                             <th>Event</th>
                             <th>Privileges</th>
                             <th>Attendance Date</th>
@@ -59,8 +59,8 @@
                             <td><?=$attendanceRow['sex']?></td>
                             <td><small><?=$attendanceRow['regDesc']." - <br>".$attendanceRow['provDesc']?></small></td>
                             <td><?=$attendanceRow['agency_name']?></td>
-                            <td><?=$attendanceRow['sectorname']?></td>
                             <td><?=$attendanceRow['position']?></td>
+                            <td><?=$attendanceRow['sectorname']?></td>
                             <td><small><?=$attendanceRow['name']?></small></td>
                             <td><?=($attendanceRow['privileges']) ? $attendanceRow['privileges'] : '-' ?></td>
                             <td><?=date("M d, Y h:i A",strtotime($attendanceRow['attendance_date'].'+8 hours'))?></td>
