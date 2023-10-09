d27a915e565d681e4f0356a59e2e624ad3c6a5dc
Ruel <rcabalunajr@gmail.com>
2023-10-09 13:55:54 +0800

    RHRC Updated

--- rhrc/app/Models/AdminModel.php
+++ rhrc/app/Models/AdminModel.php
@@ -10,7 +10,10 @@ class AdminModel extends Model
     public function get_all_data($tablename){
 
         $builder = $this->db->table($tablename);
+        $$builder->limit(1);
+        
         $query   = $builder->get();
+        
 
         return $query->getResultArray();
     }
